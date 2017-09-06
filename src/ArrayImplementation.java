e22c94b6a47bd144886dce2d40e7dca16cc8c10e
Matt Noblett <noblettm@mail.gvsu.edu>
2017-09-06 13:33:36 -0400

    Working Looping and correct string concatination

--- src/ArrayImplementation.java
+++ src/ArrayImplementation.java
@@ -27,62 +27,66 @@ public class ArrayImplementation
 		//TO DO.  Implement this method to generate the languages given in the homework description.
 		//        Implement this method with language defined as a field for a two-dimensional
 		//        array of Strings, where the rows in this.language[ row ] are of variable length.
-
-		for(int i = 0; i <= this.generations; i++){
-			for(int j = 0; j <= this.language[i].length -1; j++){
-					for(int h = 0; h <= this.alphabet.length-1; h++){
-						if(i != 0){
-							this.language[i][j] = this.language[i-1][(j%this.language[i-1].length)] + this.alphabet[j%this.alphabet.length];
-						}else{
-							this.language[i][j] = "";
-						}
-					}
+		//Set the base case.
+		this.language[0][0] = "";
+		//Loop through the size.
+		for(int i = 1; i <= this.generations; i++){
+			//Counter to increase the size of the "sliding window"
+			int step = 0;
+			//Loop through the row
+			for(int j = 0; j < this.language[i-1].length; j++){
+				//Loop through the alphabet part to create the words
+				for(int h = 0; h < this.alphabet.length; h++){
+					this.language[i][j+h+step] = this.language[i-1][j] + this.alphabet[h];
 				}
+				//Increment to increase the sliding window
+				step+=this.alphabet.length-1;
 			}
-		}
-
-		private String stringFor( String[ ] str )
-		{
-			if (str.length == 0)
-			{
-				return "{ }";
-			}
-
-			String st = "{";
-			for (String s: str)
-			{
-				st += " " + s + ",";
-			}
-			st = st.substring(0, st.length( )-1) + " }";
-			return st;
-		}
-
-		public void print()
-		{
-			System.out.println( "A language with string lengths <= " + this.generations + " over alphabet " + stringFor( this.alphabet ) );
-			for( int n = 0; n <= this.generations; n++ )
-			{
-				System.out.print( n + ": " );
-				System.out.println( this.stringFor( this.language[ n ] ) );
-			}                
-		}
-
-		public static void main( String[] args )
-		{
-			String[ ] alphabet1 = { "$" };        
-			ArrayImplementation language1 = new ArrayImplementation( alphabet1, 9 );
-			language1.print( );
-			System.out.println( );
-			System.out.println( );
-
-			String[ ] alphabet2 = { "0", "1" };       
-			ArrayImplementation language2 = new ArrayImplementation( alphabet2, 4 );
-			language2.print( );
-			System.out.println( );
-			System.out.println( );
-
-			String[ ] alphabet3 = { "a", "b", "c" };        
-			ArrayImplementation language3 = new ArrayImplementation( alphabet3, 3 );
-			language3.print( );
 		}
 	}
+
+	private String stringFor( String[ ] str )
+	{
+		if (str.length == 0)
+		{
+			return "{ }";
+		}
+
+		String st = "{";
+		for (String s: str)
+		{
+			st += " " + s + ",";
+		}
+		st = st.substring(0, st.length( )-1) + " }";
+		return st;
+	}
+
+	public void print()
+	{
+		System.out.println( "A language with string lengths <= " + this.generations + " over alphabet " + stringFor( this.alphabet ) );
+		for( int n = 0; n <= this.generations; n++ )
+		{
+			System.out.print( n + ": " );
+			System.out.println( this.stringFor( this.language[ n ] ) );
+		}                
+	}
+
+	public static void main( String[] args )
+	{
+		String[ ] alphabet1 = { "$" };        
+		ArrayImplementation language1 = new ArrayImplementation( alphabet1, 9 );
+		language1.print( );
+		System.out.println( );
+		System.out.println( );
+
+		String[ ] alphabet2 = { "0", "1" };       
+		ArrayImplementation language2 = new ArrayImplementation( alphabet2, 4 );
+		language2.print( );
+		System.out.println( );
+		System.out.println( );
+
+		String[ ] alphabet3 = { "a", "b", "c" };        
+		ArrayImplementation language3 = new ArrayImplementation( alphabet3, 3 );
+		language3.print( );
+	}
+}
--- src/ArrayListImplementation.java
+++ src/ArrayListImplementation.java
@@ -8,89 +8,98 @@
 import java.util.ArrayList;
 public class ArrayListImplementation
 {
-    private String[ ] alphabet;
-    private Object[ ] language;
-    private int       generations;
+	private String[ ] alphabet;
+	private Object[ ] language;
+	private int       generations;
 
 
-    public ArrayListImplementation( String[ ] alphabet, int generations )
-    {
-        this.alphabet    = alphabet;
-        this.generations = generations;
+	public ArrayListImplementation( String[ ] alphabet, int generations )
+	{
+		this.alphabet    = alphabet;
+		this.generations = generations;
 
-        this.language = new Object[ this.generations + 1];
-        for (int n = 0; n <= this.generations; n++ )
-        {
-            this.language[ n ] = new ArrayList<String>( );            
-        }
+		this.language = new Object[ this.generations + 1];
+		for (int n = 0; n <= this.generations; n++ )
+		{
+			this.language[ n ] = new ArrayList<String>( );            
+		}
 
-        this.generateStrings( );
-    }
-    
-
-    private void generateStrings( )
-    {
-        //TO DO.  Implement this method to generate the languages given in the homework description.
-        //        Implement this method with language defined as a field for a generic array of
-        //        rows so that this.Object[ row ] is an ArrayList of Strings.
-    	
-    	for(int i =0; i<this.generations; i++){
-    		for(int j = 0; j <= this.language.length-1; j++){
-    			for(int h = 0; h<this.alphabet.length; h++){
-    				if(i != 0){
-    					this.language[h] = this.language[i-1] + this.alphabet[j%this.alphabet.length];
-    				}else{
-    					this.language[h] = "";
-    				}
-    				
-    			}
-    			
-    		}
-    	}
-    }
-    	
+		this.generateStrings( );
+	}
 
 
-    private String stringFor( String[ ] str )
-    {
-        if (str.length == 0)
-        {
-            return "{ }";
-        }
+	private void generateStrings( )
+	{
+		//TO DO.  Implement this method to generate the languages given in the homework description.
+		//        Implement this method with language defined as a field for a generic array of
+		//        rows so that this.Object[ row ] is an ArrayList of Strings.
+		
+		//Create a blank new array list to add each piece of the words that are being created. 
+		ArrayList<String> newArray = new ArrayList<String>(1);
+		newArray.add("");
+		
+		for(int i = 0; i<= this.generations; i++){
+			if(i == 0){
+				this.language[i] = newArray;
+			}
+			else{
+				ArrayList<String> newWordArray = new ArrayList<String>();
+				//For each loop to loop through the language
+				for(String word : (ArrayList<String>)this.language[i-1]){
+					//Foreach loop to loop through the alphabet that is created
+					for(String letter : this.alphabet){
+						//Adds the new created word to the neWordArray
+						newWordArray.add(word+letter);
+					}
+				}
+				//Setting the language[i] to the newly created word
+				this.language[i] = newWordArray;
+			}	
+		}
+	}
 
-        String st = "{";
-        for (String s: str)
-        {
-            st += " " + s + ",";
-        }
-        st = st.substring(0, st.length( )-1) + " }";
-        return st;
-    }
 
-    public void print()
-    {
-        System.out.println( "A language with string lengths <= " + this.generations + " over alphabet " + stringFor( this.alphabet ) );
-        for( int n = 0; n <= this.generations; n++ )
-        {
-            System.out.print( n + ": " );
-            System.out.println( (ArrayList<String>)this.language[ n ] );
-        }                
-    }
 
-    public static void main( String[] args )
-    {
-        String[ ] alphabet1 = { "$" };        
-        ArrayListImplementation language1 = new ArrayListImplementation( alphabet1, 9 );
-        language1.print( );
-        System.out.println( );
+	private String stringFor( String[ ] str )
+	{
+		if (str.length == 0)
+		{
+			return "{ }";
+		}
 
-        String[] alphabet2 = { "0", "1" };        
-        ArrayListImplementation language = new ArrayListImplementation( alphabet2, 4 );
-        language.print( );
-        System.out.println( );
+		String st = "{";
+		for (String s: str)
+		{
+			st += " " + s + ",";
+		}
+		st = st.substring(0, st.length( )-1) + " }";
+		return st;
+	}
 
-        String[ ] alphabet3 = { "a", "b", "c" };        
-        ArrayListImplementation language3 = new ArrayListImplementation( alphabet3, 3 );
-        language3.print( );
-    }
+	public void print()
+	{
+		System.out.println( "A language with string lengths <= " + this.generations + " over alphabet " + stringFor( this.alphabet ) );
+		for( int n = 0; n <= this.generations; n++ )
+		{
+			System.out.print( n + ": " );
+			System.out.println( (ArrayList<String>)this.language[ n ] );
+		}                
+	}
+
+	public static void main( String[] args )
+	{
+		String[ ] alphabet1 = { "$" };        
+		ArrayListImplementation language1 = new ArrayListImplementation( alphabet1, 9 );
+		language1.print( );
+		System.out.println( );
+
+		String[] alphabet2 = { "0", "1" };        
+		ArrayListImplementation language = new ArrayListImplementation( alphabet2, 4 );
+		language.print( );
+		System.out.println( );
+
+		String[ ] alphabet3 = { "a", "b", "c" };        
+		ArrayListImplementation language3 = new ArrayListImplementation( alphabet3, 3 );
+		language3.print( );
+	}
 }
